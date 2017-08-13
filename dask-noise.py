0ec8c0cc9c5827374cb94bc27b5bd2fd1605d9b9
JedrzejMosiezny <jedrzej.mosiezny@gmail.com>
2017-08-13 22:12:13 +0200

    changed dast-noise.py for large files

--- dask-noise.py
+++ dask-noise.py
@@ -10,39 +10,56 @@ import matplotlib as plt
 import flask
 import math
 
+print("Starting code...")
+
 path_data = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01'
 path_post = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post'
 path_acu = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post/acu'
 path_plots = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post/plots' #ścieżka do katalogu z interesującymi nas plikami
+print("Loaded directories")
+
 
 #path_data = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/data'
 #path_post = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post' 
 #path_acu = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/acu'
 #path_plots = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/plots' #ścieżka do katalogu z interesującymi nas plikami
 
-batch_data = dd.read_csv('data/asdp_data-*.dat', sep='/s+', decimal='.')
+print("Loading batch data...")
+os.chdir(path_data)
+batch_data = dd.read_csv('int-01-*.dat', delimiter=r"\s+", decimal='.')
+print("Batch data done...")
+
 averages = pd.DataFrame(batch_data.groupby('nodenumber').mean().compute())
+print("Batch averages done...")
+
 avg_static_p = pd.DataFrame({'pressure': averages['pressure']})
 node_coords = pd.DataFrame({
     'x-coordinate': averages['x-coordinate'],
     'y-coordinate': averages['y-coordinate'],
     'z-coordinate': averages['z-coordinate']
 })
+print("Node coords done...")
 
 del(batch_data)
+print("Batch data deleted...")
 
+print("Listing files...")
 filelist = os.listdir(path_data)
 
+print("Starting noise analysis loop...")
 for file in filelist:
     os.chdir(path_data)
-    timestep = str(os.path.basename(str(file)))[10:-4]
+    timestep = str(os.path.basename(str(file)))[7:-4]
     time_static_p = pd.DataFrame(pd.read_csv(file, sep='/s+', header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
     acoustic_p = time_static_p.subtract(avg_static_p, fill_value=None)
     db = acoustic_p.apply(lambda x: 20 * np.log10(np.abs(x)/0.00002), axis=1)
     acoustic_data = pd.concat([node_coords, acoustic_p, db], axis=1)
     os.chdir(path_acu)
     acoustic_p.to_csv(str('asdp_acu_p_' + str(timestep) + '.dat'), sep=',')
-    
+
+print("Exiting noise analysis loop...")
+
+input("Script done... Press any key to exit")
     
 
 

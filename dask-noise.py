cb2a3f47a67e1ccadd2642bde2694e6f0a5ffde4
JedrzejMosiezny <jedrzej.mosiezny@gmail.com>
2017-08-13 21:02:16 +0200

    commit new branch

--- dask-noise.py
+++ dask-noise.py
@@ -10,12 +10,17 @@ import matplotlib as plt
 import flask
 import math
 
-path_data = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/data'
-path_post = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post' 
-path_acu = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/acu'
-path_plots = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/plots' #ścieżka do katalogu z interesującymi nas plikami
+path_data = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01'
+path_post = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post'
+path_acu = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post/acu'
+path_plots = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/13_PLGRID/noise-data/int-01/post/plots' #ścieżka do katalogu z interesującymi nas plikami
 
-batch_data = dd.read_csv('data/asdp_data-*.dat', sep='\s+', decimal='.')
+#path_data = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/data'
+#path_post = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post' 
+#path_acu = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/acu'
+#path_plots = 'D:/01_Dokumenty/01_PUT/01_DOKTORAT/11_CFX/05_PhD runs/R67_fluent/peak_R67_hgt_512k/trn_des_v01/post/plots' #ścieżka do katalogu z interesującymi nas plikami
+
+batch_data = dd.read_csv('data/asdp_data-*.dat', sep='/s+', decimal='.')
 averages = pd.DataFrame(batch_data.groupby('nodenumber').mean().compute())
 avg_static_p = pd.DataFrame({'pressure': averages['pressure']})
 node_coords = pd.DataFrame({
@@ -31,7 +36,7 @@ filelist = os.listdir(path_data)
 for file in filelist:
     os.chdir(path_data)
     timestep = str(os.path.basename(str(file)))[10:-4]
-    time_static_p = pd.DataFrame(pd.read_csv(file, sep='\s+', header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
+    time_static_p = pd.DataFrame(pd.read_csv(file, sep='/s+', header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
     acoustic_p = time_static_p.subtract(avg_static_p, fill_value=None)
     db = acoustic_p.apply(lambda x: 20 * np.log10(np.abs(x)/0.00002), axis=1)
     acoustic_data = pd.concat([node_coords, acoustic_p, db], axis=1)
@@ -43,4 +48,4 @@ for file in filelist:
 
 
 
-rms = batch_data.groupby('nodenumber').square().compute()
+#rms = batch_data.groupby('nodenumber').square().compute()

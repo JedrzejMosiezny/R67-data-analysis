52abd2971db5d5eabf245e86c25c331bb6241496
JedrzejMosiezny <jedrzej.mosiezny@gmail.com>
2017-08-14 18:29:41 +0200

    changed for final filenames

--- dask-noise.py
+++ dask-noise.py
@@ -50,12 +50,13 @@ print("Starting noise analysis loop...")
 for file in filelist:
     os.chdir(path_data)
     timestep = str(os.path.basename(str(file)))[7:-4]
-    time_static_p = pd.DataFrame(pd.read_csv(file, sep='/s+', header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
+    #time_static_p = pd.DataFrame(pd.read_csv(file, sep='/s+', header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
+    time_static_p = pd.DataFrame(pd.read_csv(file, delimiter=r"\s+", header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
     acoustic_p = time_static_p.subtract(avg_static_p, fill_value=None)
     db = acoustic_p.apply(lambda x: 20 * np.log10(np.abs(x)/0.00002), axis=1)
     acoustic_data = pd.concat([node_coords, acoustic_p, db], axis=1)
     os.chdir(path_acu)
-    acoustic_p.to_csv(str('asdp_acu_p_' + str(timestep) + '.dat'), sep=',')
+    acoustic_p.to_csv(str('int-01_acu_p_' + str(timestep) + '.dat'), sep=',')
 
 print("Exiting noise analysis loop...")
 

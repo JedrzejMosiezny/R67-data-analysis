360b50dea5005bc08d61d81ee66687c7b0209e8e
JedrzejMosiezny <jedrzej.mosiezny@gmail.com>
2017-09-30 22:37:02 +0200

    added noise intensity calc

--- dask-noise-internals.py
+++ dask-noise-internals.py
@@ -36,14 +36,18 @@ print("Calculating batch averages...")
 averages = pd.DataFrame(batch_data.groupby('nodenumber').mean().compute())
 print("Batch averages done...")
 
-print("Generating node coordinates...")
+print("Generating average pressure dataframe")
 avg_static_p = pd.DataFrame({'pressure': averages['pressure']})
+
+print("Generating average velocity dataframe")
+avg_rel_v = pd.DataFrame({'rel-velocity-magnitude': averages['rel-velocity-magnitude']})
+
+print("Generating node coordinates...")
 node_coords = pd.DataFrame({
     'x-coordinate': averages['x-coordinate'],
     'y-coordinate': averages['y-coordinate'],
     'z-coordinate': averages['z-coordinate']
 })
-print("Node coords done...")
 
 del(batch_data)
 print("Batch data deleted...")
@@ -57,9 +61,13 @@ for file in filelist:
     timestep = str(os.path.basename(str(file)))[7:-4]
     time_static_p = pd.DataFrame(pd.read_csv(file, delimiter=r"\s+", header=0, usecols=["nodenumber", "pressure"], skiprows=0, decimal='.')).set_index('nodenumber')
     acoustic_p = time_static_p.subtract(avg_static_p, fill_value=None)
-    db = acoustic_p.apply(lambda x: 20 * np.log10(np.abs(x)/0.00002), axis=1)
-    acoustic_data = pd.concat([node_coords, acoustic_p, db], axis=1)
-    acoustic_data.columns = ['x-coordinate', 'y-coordinate', 'z-coordinate', 'sound-pressure', 'db-level']
+    spl_db = acoustic_p.apply(lambda x: 20 * np.log10(np.abs(x)/0.00002), axis=1)
+    time_rel_vel = pd.DataFrame(pd.read_csv(file, delimiter=r"\s+", header=0, usecols=["nodenumber", "rel-velocity-magnitude"], skiprows=0, decimal='.')).set_index('nodenumber')
+    particle_v = time_rel_vel.subtract(avg_rel_v, fill_value=None).abs()
+    sound_intensity = pd.DataFrame(acoustic_p.values*particle_v.values, index=acoustic_p.index)
+    sil_db = sound_intensity.apply(lambda x: 10 * np.log10(np.abs(x)/1e-12), axis=1)
+    acoustic_data = pd.concat([node_coords, acoustic_p, spl_db, particle_v, sound_intensity, sil_db], axis=1)
+    acoustic_data.columns = ['x-coordinate', 'y-coordinate', 'z-coordinate', 'sound-pressure', 'spl-db', 'particle-velocity', 'sound-intensity', 'sil-db']
     os.chdir(path_acu)
     acoustic_data.to_csv(str('int-01_acu_' + str(timestep) + '.dat'), sep=',')
     print(str('int-01_acu_' + str(timestep) + '.dat done...'))

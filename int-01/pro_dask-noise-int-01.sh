b3800f52dd61a25cf584f6f9f099d8baf2c66bdb
JedrzejMosiezny <jedrzej.mosiezny@gmail.com>
2017-10-11 10:06:20 +0200

    path fix in post int-01

--- int-01/pro_dask-noise-int-01.sh
+++ int-01/pro_dask-noise-int-01.sh
@@ -29,6 +29,6 @@ module load tools/python-intel/3.6.2
 #cd $PBS_O_WORKDIR
 
 ## run calculation
-python3 /net/people/plgmosieznyj/noise-analysis/int-01/dask-noise-int-01-pro.py
+python3 /net/people/plgmosieznyj/SRS_v02/noise-analysis/int-01/dask-noise-int-01-pro.py
 
 # ----------------------------------------------------------------- end-of-file

228a6cc338f3ed05474c9842134b44398a8a69c8
Jedrzej Mosiezny <jedrzej.mosiezny@gmail.com>
2018-07-19 22:17:07 +0200

    pro tasks setup

--- int-03/pro_fft_int-03.sh
+++ int-03/pro_fft_int-03.sh
@@ -6,13 +6,13 @@
 ## Ilość zadań na węzeł
 #SBATCH --ntasks-per-node=1
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
-#SBATCH --mem-per-cpu=48GB
+#SBATCH --mem-per-cpu=24GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=72:00:00 
+#SBATCH --time=168:00:00 
 ## Nazwa grantu do rozliczenia zużycia zasobów
 #SBATCH -A acnoise2017
 ## Specyfikacja partycji
-#SBATCH -p plgrid
+#SBATCH -p plgrid-long
 ## Plik ze standardowym wyjściem
 #SBATCH --output="fft.out"
 ## Plik ze standardowym wyjściem błędów
--- tip/pro_fft_tip.sh
+++ tip/pro_fft_tip.sh
@@ -6,9 +6,9 @@
 ## Ilość zadań na węzeł
 #SBATCH --ntasks-per-node=1
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
-#SBATCH --mem-per-cpu=48GB
+#SBATCH --mem-per-cpu=24GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=72:00:00 
+#SBATCH --time=168:00:00 
 ## Nazwa grantu do rozliczenia zużycia zasobów
 #SBATCH -A acnoise2017
 ## Specyfikacja partycji
